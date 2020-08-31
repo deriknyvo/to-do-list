06a370a959d50f3f5a59724d0d81d0cb2d3b1c89
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2020-08-30 22:33:33 -0300

    Refatorado para um unico componente afim de fazer funcionar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import './App.css';
 
-import Input from './components/Input/index';
-import List from './components/List/index';
+const App = () => {
+  const [item, setItem] = React.useState('');
+  const [list, setList] = React.useState([]);
 
-function App() {
-  const [itemsList = [], setItemList] = React.useState();
+  function addItem(item) {
+    if (item.match(/\w/)) {
+      item = item.trim();
+      const newList = list.concat(item);
+      setList(newList);
+      setItem('');
+    }
+  }
+
+  function onChangeValue(event) {
+    setItem(event.target.value);
+  }
 
   return (
-    <div className="container">
-      <Input setItemList={setItemList} />
-      <List itemsList={itemsList} />
+    <div className="todo">
+      <div> 
+        <label htmlFor="input">Digite aqui...</label>
+        <input 
+          value={item} 
+          onChange={onChangeValue}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              addItem(item);
+            }
+          }}
+          type="text" 
+          id="input" 
+        />
+        <button type="button" onClick={() => addItem(item)}>Salvar</button>
+      </div>
+
+      <div>
+        <ul>
+          {list.map((item, index) => {
+            return (
+              <li key={index}>{item}</li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
